6c95165bc0159c095c69dcd2c0d36f7f5ad50790
Junayet Khan <junayetkhanjon@gmail.com>
2022-11-13 07:18:28 -0800

    js added

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-
-
 document.getElementById('quantity-plus-btn').addEventListener("click",function(){
     const quantityField = document.getElementById('quantity-input');
     const getquantityAmmount = quantityField.value;
-    const quantityAmmount = parseInt(getquantityAmmount);
+    const quantityAmmount = parseFloat(getquantityAmmount);
     const totallAmmount =  quantityAmmount +1;
     quantityField.value = totallAmmount;
 
@@ -12,12 +10,44 @@ document.getElementById('quantity-plus-btn').addEventListener("click",function()
    const totalliphoneprice = totallAmmount * 1219;
    iphonepriceField.innerText = totalliphoneprice;
     
-})
+});
 document.getElementById('quantity-less-btn').addEventListener("click",function(){
     const quantityField = document.getElementById('quantity-input');
     const getquantityAmmount = quantityField.value;
-    const quantityAmmount = parseInt(getquantityAmmount);
+    const quantityAmmount = parseFloat(getquantityAmmount);
     const totallAmmount =  quantityAmmount -1;
     quantityField.value = totallAmmount;
+    const iphonepriceField = document.getElementById('iphone-price');
+   const getiphonepriceField = iphonepriceField.innerText;
+   const totalliphoneprice = totallAmmount * 1219;
+   iphonepriceField.innerText = totalliphoneprice;
+    
+    
+});
+
+document.getElementById('cover-plus-btn').addEventListener("click",function(){
+    const quantityField = document.getElementById('cover-quantity-input');
+    const getquantityAmmount = quantityField.value;
+    const quantityAmmount = parseFloat(getquantityAmmount);
+    const totallAmmount =  quantityAmmount +1;
+    quantityField.value = totallAmmount;
+
+   const iphonepriceField = document.getElementById('cover-price');
+   const getiphonepriceField = iphonepriceField.innerText;
+   const totalliphoneprice = totallAmmount * 59;
+   iphonepriceField.innerText = totalliphoneprice;
+    
+});
+document.getElementById('cover-less-btn').addEventListener("click",function(){
+    const quantityField = document.getElementById('cover-quantity-input');
+    const getquantityAmmount = quantityField.value;
+    const quantityAmmount = parseFloat(getquantityAmmount);
+    const totallAmmount =  quantityAmmount -1;
+    quantityField.value = totallAmmount;
+    const iphonepriceField = document.getElementById('cover-price');
+   const getiphonepriceField = iphonepriceField.innerText;
+   const totalliphoneprice = totallAmmount * 59;
+   iphonepriceField.innerText = totalliphoneprice;
+    
     
 });
